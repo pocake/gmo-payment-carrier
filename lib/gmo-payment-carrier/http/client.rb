576ef4a104d8e66e1e39e9d4181ef02bb6c436a5
fukuiretu <s0232101@gmail.com>
2015-10-30 19:16:25 +0900

    Add http module

--- lib/gmo-payment-carrier/http/client.rb
+++ lib/gmo-payment-carrier/http/client.rb
@@ -1,6 +1,68 @@
 module GMOPaymentCarrier
   module Http
     class Client
+      DEFAULT_HEADERS = {
+        "Accept" => "application/json",
+        "User-Agent" => "Piese of Cake"
+      }.freeze
+
+      def initialize(endpoint:, request: :url_encoded, response: :logger)
+        @endpoint = endpoint
+        @request  = request
+        @response = response
+      end
+
+      def connection
+        @connection ||= Faraday.new(faraday_client_options) do |connection|
+          connection.request @request if @request.present?
+          connection.response @response if @Response.present?
+          connection.adapter Faraday.default_adapter
+        end
+      end
+
+      def get(path:, params: nil, headers: nil)
+        process(:get, path, params, headers)
+      end
+
+      def post(path:, params: nil, headers: nil)
+        process(:post, path, params, headers)
+      end
+
+      def patch(path:, params: nil, headers: nil)
+        process(:patch, path, params, headers)
+      end
+
+      def put(path:, params: nil, headers: nil)
+        process(:put, path, params, headers)
+      end
+
+      def delete(path:, params: nil, headers: nil)
+        process(:delete, path, params, headers)
+      end
+
+      private
+
+        def process(request_method, path, params, headers)
+          Response.new(
+            connection.send(
+              request_method,
+              URI.escape(path),
+              params,
+              headers,
+            )
+          )
+        end
+
+        def default_headers
+          DEFAULT_HEADERS.clone
+        end
+
+        def faraday_client_options
+          {
+            headers: default_headers,
+            url: @endpoint,
+          }
+        end
     end
   end
 end
--- lib/gmo-payment-carrier/http/response.rb
+++ lib/gmo-payment-carrier/http/response.rb
@@ -1,6 +1,29 @@
 module GMOPaymentCarrier
   module Http
     class Response
+      def initialize(faraday_response)
+        @raw_body = faraday_response.body
+        @raw_headers = faraday_response.headers
+        @raw_status = faraday_response.status
+      end
+
+      def body
+        @raw_body
+      end
+
+      def headers
+        @headers ||= @raw_headers.inject({}) do |result, (key, value)|
+          result.merge(key.split("-").map(&:capitalize).join("-") => value)
+        end
+      end
+
+      def status
+        @raw_status
+      end
+
+      def status_message
+        Rack::Utils::HTTP_STATUS_CODES[status]
+      end
     end
   end
 end

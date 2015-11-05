9a3b4aa3aca0af6b98bb7b5720e4ee29ed826f3e
fukuiretu <s0232101@gmail.com>
2015-11-05 17:54:36 +0900

    modify spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,6 @@ require "pry"
 require "ffaker"
 require 'simplecov'
 require "codeclimate-test-reporter"
-
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-  SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
-]
-SimpleCov.start
 CodeClimate::TestReporter.start
 
 RSpec.configure do |config|

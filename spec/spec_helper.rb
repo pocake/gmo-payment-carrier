56ce0d4265f7e9059f81c044eac62e203937dadf
fukuiretu <s0232101@gmail.com>
2015-11-05 17:45:51 +0900

    Modify spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,14 @@
 require "gmo-payment-carrier"
 require "pry"
 require "ffaker"
+require 'simplecov'
 require "codeclimate-test-reporter"
 
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  CodeClimate::TestReporter::Formatter
+]
+SimpleCov.start
 CodeClimate::TestReporter.start
 
 RSpec.configure do |config|

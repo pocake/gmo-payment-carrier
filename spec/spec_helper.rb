3339922e8a66317ce93f1e8ae0a15affaf660efe
hiroya.shimamoto <shshimamo@gmail.com>
2020-09-09 01:02:14 +0900

    Remove codeclimate-test-reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require "gmo-payment-carrier"
 require "pry"
 require "ffaker"
